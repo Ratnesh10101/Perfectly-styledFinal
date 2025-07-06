0ec66dac87d5444be69e927e1639ea9d68be7851
vickyks <vickyks@hotmail.com>
2025-07-06 14:59:10 +0000

    removed unused functions

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * Import function triggers from their respective submodules:
  *
@@ -6,11 +7,11 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
-
 // export const helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+//# sourceMappingURL=index.js.map
